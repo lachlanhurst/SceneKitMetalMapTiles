b642bea1bbf4e2748bddc650ddf9141b2b7a3d84
Lachlan Hurst <lachlan@vpac.org>
2017-03-11 16:11:12 +1100

    Initial Commit

---
+++ SceneKitMetalMaptilesTests/SceneKitMetalMaptilesTests.swift
@@ -0,0 +1,36 @@
+//
+//  SceneKitMetalMaptilesTests.swift
+//  SceneKitMetalMaptilesTests
+//
+//  Created by Lachlan Hurst on 11/3/17.
+//  Copyright © 2017 Lachlan Hurst. All rights reserved.
+//
+
+import XCTest
+@testable import SceneKitMetalMaptiles
+
+class SceneKitMetalMaptilesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
